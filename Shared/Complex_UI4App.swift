a29636990e55cf77019af0c42088e6cc386fa381
sikandarali307 <sikandar.ali307@gmail.com>
2021-11-01 12:02:23 +0500

    Initial Commit

---
+++ Shared/Complex_UI4App.swift
@@ -0,0 +1,17 @@
+//
+//  Complex_UI4App.swift
+//  Shared
+//
+//  Created by Mac on 01/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct Complex_UI4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
